8dcf5fad73f43360251697a077b251190cbd0a90
Sana Ullah Siddiqui <sanaullah.siddiqui0317@gmail.com>
2026-04-26 02:45:55 +0500

    in this type of production updated code and bad practice

--- index.js
+++ index.js
@@ -1,55 +1,38 @@
-const express = require('express');
+import express from "express";
+// import dotenv from "dotenv";
+// dotenv.config();
 
 const app = express();
-const port = 3000;
 
-const githubApi = {
-    "login": "SanaUllahCoder",
-    "id": 156604999,
-    "node_id": "U_kgDOCVWaRw",
-    "avatar_url": "https://avatars.githubusercontent.com/u/156604999?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/SanaUllahCoder",
-    "html_url": "https://github.com/SanaUllahCoder",
-    "followers_url": "https://api.github.com/users/SanaUllahCoder/followers",
-    "following_url": "https://api.github.com/users/SanaUllahCoder/following{/other_user}",
-    "gists_url": "https://api.github.com/users/SanaUllahCoder/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/SanaUllahCoder/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/SanaUllahCoder/subscriptions",
-    "organizations_url": "https://api.github.com/users/SanaUllahCoder/orgs",
-    "repos_url": "https://api.github.com/users/SanaUllahCoder/repos",
-    "events_url": "https://api.github.com/users/SanaUllahCoder/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/SanaUllahCoder/received_events",
-    "type": "User",
-    "user_view_type": "public",
-    "site_admin": false,
-    "name": "Sana Ullah Siddiqui",
-    "company": null,
-    "blog": "https://www.shamsifytools.com",
-    "location": null,
-    "email": null,
-    "hireable": null,
-    "bio": "I am Frontend Web developer | and Founder of Shamsify Tools",
-    "twitter_username": null,
-    "public_repos": 48,
-    "public_gists": 0,
-    "followers": 1,
-    "following": 0,
-    "created_at": "2024-01-14T17:51:47Z",
-    "updated_at": "2026-04-17T12:16:16Z"
-}
 
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-})
+app.use(express.static('dist'))
 
-app.get('/mygithub', (req, res) => {
-    res.json(githubApi)
 
-})
+app.get("/api/student", (req, res) => {
+  const students = [
+    { id: 1, name: "Ali", age: 20 },
+    { id: 2, name: "Ahmed", age: 21 },
+    { id: 3, name: "Sara", age: 19 },
+    { id: 4, name: "Usman", age: 22 },
+    { id: 5, name: "Ayesha", age: 20 },
+    { id: 6, name: "Bilal", age: 23 },
+    { id: 7, name: "Hina", age: 18 },
+    { id: 8, name: "Zain", age: 24 },
+    { id: 9, name: "Fatima", age: 21 },
+    { id: 10, name: "Hassan", age: 22 },
+    { id: 11, name: "Maryam", age: 19 },
+    { id: 12, name: "Omar", age: 25 },
+    { id: 13, name: "Noor", age: 20 },
+    { id: 14, name: "Daniyal", age: 23 },
+    { id: 15, name: "Laiba", age: 18 },
+    { id: 15, name: "Laiba", age: 18 }
 
+  ];
+  res.send(students);
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`server already start on this port ${port}`)
-
-})
+  console.log(`START SERVER on port`);
+});
